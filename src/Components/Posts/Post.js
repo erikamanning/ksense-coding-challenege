ff3f0e873f334306d018dfee4d53170e7dbef677
erika <erikamanning@protonmail.com>
2021-11-11 16:51:24 -0700

    finished post component with styling and appropriate data

--- src/Components/Posts/Post.js
+++ src/Components/Posts/Post.js
@@ -1,12 +1,33 @@
-import React from "react"
+import React, {useContext} from "react"
+import {UserContext} from '../Users/User'
+import { getRandImage } from "../../imageHelpers"
 
-const Post = ({title}) => {
+const Post = ({post}) => {
 
+    const {user} = useContext(UserContext);
+    const randImage = getRandImage();
 
     return (
-        <div>
-            <h1>{title}</h1>
+
+        <div className="card mb-2">
+            <div className="card-content">
+                <div className="media">
+                    <div className="media-left">
+                        <figure className="image is-48x48">
+                            <img src={randImage} alt='rand-scenery'/>
+                        </figure>
+                    </div>
+                    <div className="media-content">
+                        <p className="title is-4 is-capitalized">{post.title}</p>
+                        <p className="subtitle is-6">@{user.username}</p>
+                    </div>
+                </div>
+                <div className="content">
+                    {post.body}
+                </div>
+            </div>
         </div>
+
     )
 }
 
