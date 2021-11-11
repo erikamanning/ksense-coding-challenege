7041fab867a1c4139e481029dd43e6b0b981ebfd
erika <erikamanning@protonmail.com>
2021-11-11 13:32:28 -0700

    added dummy data to user posts page

--- src/Components/Users/User.js
+++ src/Components/Users/User.js
@@ -1,11 +1,16 @@
 import React from "react"
+import { useParams } from "react-router";
+import PostList from '../Posts/PostList'
 
 const User = () => {
 
+    const {username} = useParams();
+    console.log('username: ', username);
 
     return (
         <div>
-
+            <h1>{username}'s posts</h1>
+            <PostList />
         </div>
     )
 }
