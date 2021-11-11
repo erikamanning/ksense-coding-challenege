434ed1e6b0122e3f4640de2457f4aa37cab25164
erika <erikamanning@protonmail.com>
2021-11-11 13:48:06 -0700

    wrote useEffect to grab api data on component load

--- src/Components/Users/UserList.js
+++ src/Components/Users/UserList.js
@@ -1,9 +1,25 @@
-import React from "react"
+import React, { useEffect } from "react"
 import UserListItem from './UserListItem'
 import { v4 as uuidv4 } from 'uuid';
 import { Outlet } from "react-router-dom";
+import AppDataApi from "../../AppDataApi";
 
 const UserList = ({users}) => {
+
+    let userData;
+
+    useEffect(()=>{
+
+        async function getUserData(){
+
+            let userData = await AppDataApi.getUsers();
+            console.log('USERLIST -- userData: ', userData);
+        }
+
+        getUserData();
+
+    },[])
+
 
     return (
         <div className="container">
