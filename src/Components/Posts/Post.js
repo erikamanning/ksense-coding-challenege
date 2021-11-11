37006e42a810ebdaa184505de7b35ef95df86947
erika <erikamanning@protonmail.com>
2021-11-11 13:32:51 -0700

    added skeleton and dummy data for displaying posts list

--- src/Components/Posts/Post.js
+++ src/Components/Posts/Post.js
@@ -1,11 +1,11 @@
 import React from "react"
 
-const Post = () => {
+const Post = ({title}) => {
 
 
     return (
         <div>
-
+            <h1>{title}</h1>
         </div>
     )
 }
--- src/Components/Posts/PostList.js
+++ src/Components/Posts/PostList.js
@@ -1,10 +1,29 @@
 import React from "react"
+import { v4 as uuidv4 } from 'uuid';
+import Post from './Post'
 
 const PostList = () => {
+
+    const posts = [
+        {
+            title:'Favorite Dogs'
+        },
+        {
+            title:'Favorite Cats'
+        },
+        {
+            title:'Favorite Birds'
+        }
+    ]
 
 
     return (
         <div>
+            Posts:
+
+            <ul>
+                {posts.map(post=>(<Post key={uuidv4()} title={post.title}/>))}
+            </ul>
 
         </div>
     )
