02958033f0ffc4faaafe8a3374d1c3a417bedad3
erika <erikamanning@protonmail.com>
2021-11-11 13:47:46 -0700

    created function to get user data from api

--- src/AppDataApi.js
+++ src/AppDataApi.js
@@ -1,7 +1,20 @@
-
+import axios from "axios";
+const API_URL = 'https://jsonplaceholder.typicode.com/'
 
 class AppDataApi{
 
-    
+    static async getUsers(){
+        try{
+
+            let resp = await axios.get(`${API_URL}users`);
+            console.log('RESPONSE: ', resp);
+            return resp.data;
+        }
+        catch(error){
+            console.log('ERROR: ', error);
+        }
+    }
 
 }
+
+export default AppDataApi;
