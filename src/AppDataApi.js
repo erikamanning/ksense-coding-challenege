828614e021a11672c6f4764a619ab9cddecdfcfb
erika <erikamanning@protonmail.com>
2021-11-11 16:51:04 -0700

    added remaining api data functions

--- src/AppDataApi.js
+++ src/AppDataApi.js
@@ -7,7 +7,26 @@ class AppDataApi{
         try{
 
             let resp = await axios.get(`${API_URL}users`);
-            console.log('RESPONSE: ', resp);
+            return resp.data;
+        }
+        catch(error){
+            console.log('ERROR: ', error);
+        }
+    }
+    static async getUserData(userId){
+        try{
+
+            let resp = await axios.get(`${API_URL}users/${userId}`);
+            return resp.data;
+        }
+        catch(error){
+            console.log('ERROR: ', error);
+        }
+    }
+    static async getUserPosts(userId){
+        try{
+
+            let resp = await axios.get(`${API_URL}users/${userId}/posts`);
             return resp.data;
         }
         catch(error){
